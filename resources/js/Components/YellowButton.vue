bace13a1283ec441dc42d2e783a7d38b823490aa
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-08-07 14:51:57 +0530

    Create laravel breeze project

---
+++ resources/js/Components/YellowButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="inline-flex items-center px-4 py-2 bg-[#f5f535] dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-black dark:text-white uppercase tracking-widest hover:bg-[#ffff00] dark:hover:bg-gray-200 focus:bg-[#ffff00] dark:focus:bg-gray-200 active:bg-[#ffff00] dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+    >
+        <slot />
+    </button>
+</template>
