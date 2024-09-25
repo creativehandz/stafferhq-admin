e06e8d47c5eee7f9852c14494219a6226086ee9f
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-09-25 14:02:53 +0530

    create a blank become a seller page

---
+++ resources/js/Pages/BecomeASeller.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import BreadcrumbDefault from "@/Components/Breadcrumbs/BreadcrumbDefault.vue";
+import { Head } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+const pageTitle = ref("Become a seller");
+</script>
+
+<template>
+    <Head title="Become a seller" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight"
+            >
+                Become a seller
+            </h2>
+        </template>
+
+        <BreadcrumbDefault :pageTitle="pageTitle" />
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div
+                    class="bg-white dark:bg-boxdark overflow-hidden shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        Become a seller page
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
