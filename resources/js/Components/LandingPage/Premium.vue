f93f59ad0ebf76d3ef819a0faeab1bc066089b1d
Manvendra2000 <mzealpro@gmail.com>
2024-10-04 19:12:47 +0530

    Home page, padding, fonts, css, slider, etc

--- resources/js/Components/LandingPage/Premium.vue
+++ resources/js/Components/LandingPage/Premium.vue
@@ -1,16 +1,15 @@
 <template>
-<div class="container px-4 py-8 mx-auto">  
-        <div class="px-6 py-12 text-black rounded-3xl md:px-16 bg-cyan-200">
-            <h2 class="mb-2 font-bold">ToWork Premium.</h2>
-            <p class="mb-4 text-lg font-semibold">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                <br>
+<div class="container px-6 py-12 mx-auto mt-10 mb-10">  
+        <div class="px-6 py-10 text-black rounded-3xl md:px-16 bg-cyan-200">
+            <h2 class="mb-6 font-bold leading-none sm:text-[55px]  md:text-[55px] lg:text-[55px] xl:text-[55px] 2xl:text-[55px]">ToWork Premium.</h2>
+            <h3 class="mb-6 text-lg font-semibold leading-none sm:text-[18px]  md:text-[18px] lg:text-[18px] xl:text-[18px] 2xl:text-[18px] w-100">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.                 
                 Etiam ac vulputate nisi.
-            </p>
-            <p class="mb-6 text-gray-700 pre-para">
+            </h3>
+            <p class="mb-6 text-gray-700 pre-para leading-none sm:text-[18px]  md:text-[18px] lg:text-[18px] xl:text-[18px] 2xl:text-[18px]">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac vulputate nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac vulputate nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac vulputate nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac vulputate nisi.
             </p>
-            <button class="px-16 py-7 font-bold text-black transition-all duration-300 bg-[#F5F535] rounded-full shadow-lg hover:bg-yellow-500">
+            <button class="px-16 py-7 font-bold text-black transition-all duration-300 bg-[#F5F535] rounded-full shadow-lg hover:bg-yellow-500 button-get-started">
                 Get started!
             </button>            
       </div>
@@ -20,26 +19,3 @@
 <script setup lang="ts">
   // No additional logic needed for this template
 </script>
-  
-<style scoped>
-  h2 {
-    font-family: 'Crimson Pro', serif;
-    font-size: 80px;
-    line-height: 96px;
-    
-  }
-  p {
-    font-family: Neue Montreal bold;
-    font-size: 24px;
-  }
-  .pre-para {
-    font-family: Neue Montreal regular;
-    font-size: 24px;
-  }
-  button {
-    font-family: Neue Montreal bold;
-    font-size: 24px;
-    
-  }
-</style>
-  
